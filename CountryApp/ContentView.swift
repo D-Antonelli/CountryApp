e385e52fcd3a8382bb2d04ddb647061528d30905
derya <derya@boonandmoil.dev>
2022-10-06 10:43:06 +0100

    Add existing project files to Git

--- CountryApp/ContentView.swift
+++ CountryApp/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var countries = Countries()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
---
+++ CountryApp/Country.swift
@@ -0,0 +1,12 @@
+//
+//  Country.swift
+//  CountryApp
+//
+//  Created by Derya Antonelli on 06/10/2022.
+//
+
+import Foundation
+
+struct Country {
+    let name: String
+}
