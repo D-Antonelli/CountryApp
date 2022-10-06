131961ba4ccf24a106ad900a6d48f7ee7ebde667
derya <derya@boonandmoil.dev>
2022-10-06 09:48:17 +0100

    Initial Commit

---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by Derya Antonelli on 06/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
